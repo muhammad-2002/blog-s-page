48440bf22badbbaf609ee4bac7a7ffa5cec661af
muhammad-2002 <masumbilla10104@gmail.com>
2024-11-03 09:36:03 +0600

    Header add

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,60 @@
-import React from "react";
+import { Button, Navbar, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { Link, useLocation } from "react-router-dom";
 
-export default function Header() {
-  return <div>Header</div>;
-}
+const Header = () => {
+  const path = useLocation().pathname;
+  const [theme, setTheme] = useState(false);
+  const handleTheme = () => {
+    setTheme(!theme);
+  };
+  return (
+    <Navbar className="border-b-2">
+      <Link
+        to={"/"}
+        className="self-center font-semibold whitespace-nowrap  text-sm sm:text-xl dark:text-white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+          Sahand's
+        </span>
+        Blog
+      </Link>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search"
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        ></TextInput>
+      </form>
+      <Button className="w-12 h-10 lg:hidden" pill color="gray">
+        <AiOutlineSearch></AiOutlineSearch>
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button
+          onClick={handleTheme}
+          className="w-12 h-10 hidden sm:inline"
+          pill
+          color="gray"
+        >
+          {theme ? <FaMoon></FaMoon> : <FaSun></FaSun>}
+        </Button>
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default Header;
--- client/src/router/Router.jsx
+++ client/src/router/Router.jsx
@@ -1,9 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
+import About from "../pages/About";
+import Home from "../pages/Home";
+import Projects from "../pages/Projects";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/projects",
+        element: <Projects></Projects>,
+      },
+    ],
   },
 ]);
