94da951a1bd7913ac6bc9beb1df3f1200a6af3e3
muhammad-2002 <masumbilla10104@gmail.com>
2024-11-03 14:10:39 +0600

    fix issue

--- api/index.js
+++ api/index.js
@@ -1,12 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
-<<<<<<< HEAD
 import authRouter from "./routers/auth.router.js";
 import userRouter from "./routers/user.router.js";
-=======
-import UserRouter from "./routers/user.router.js";
->>>>>>> b34d5c528367a8223ca72ee17a8a27ca8856131c
 const app = express();
 dotenv.config()
 app.use(express.json())
@@ -22,9 +18,5 @@ mongoose.connect(process.env.URI).then(()=>{
 
 
 app.listen(port,()=>console.log(`listening Port ${port}`))
-<<<<<<< HEAD
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
-=======
-app.use('/api/user',UserRouter)
->>>>>>> b34d5c528367a8223ca72ee17a8a27ca8856131c
